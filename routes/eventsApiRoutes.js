ca9373e94e530724f170378bd71f5d1a816eb5c6
danieskka <daniescalera93@hotmail.com>
2023-06-19 18:58:27 +0200

    fix/routes

--- routes/eventsApiRoutes.js
+++ routes/eventsApiRoutes.js
@@ -2,22 +2,28 @@ const express = require('express');
 const eventsApiRouter = express.Router();
 const eventsApiController = require('../controllers/eventsApiController');
 
+// Rutas /user
 eventsApiRouter.post('/user', eventsApiController.registerProfile);
 eventsApiRouter.put('/user', eventsApiController.editProfile);
 eventsApiRouter.delete('/user', eventsApiController.deleteProfile);
 
+// Rutas login/out
 eventsApiRouter.post('/login', eventsApiController.userLogin);
 eventsApiRouter.post('/logout', eventsApiController.userLogout);
 
+// Ruta /search
 eventsApiRouter.get('/search', eventsApiController.getEvents);
 
+// Rutas /ads
 eventsApiRouter.post('/ads', eventsApiController.createEvent);
 eventsApiRouter.put('/ads', eventsApiController.editEvent);
 eventsApiRouter.delete('/ads', eventsApiController.deleteEvent);
 
+// Rutas /favorites
 eventsApiRouter.post('/favorites', eventsApiController.addFavorite);
 eventsApiRouter.delete('/favorites', eventsApiController.deleteFavorite);
 
+// Rutas password
 eventsApiRouter.get('/recoverpassword', eventsApiController.recoverPass);
 eventsApiRouter.get('/restorepassword', eventsApiController.restorePass);
 
