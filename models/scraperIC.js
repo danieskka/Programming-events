c5b43f90f1f6b078061d8fa9223669c16dcdaafb
Rebeca <becabecks3.gmail.com>
2023-06-20 23:37:14 +0200

    add/data-db

--- models/scraperIC.js
+++ models/scraperIC.js
@@ -1,48 +1,66 @@
 const mongoose = require('mongoose');
 require('../utils/db_mongo') 
+const fs = require('fs');
 
-const objectSchema = {
-    id: { 
-        type: Number, 
-        required: true,
-        unique: true
-    },
-    title: { 
-        type: String, 
-        required: true,
-        unique: true 
-    },
-    price: { 
-        type: Number, 
-        required: true 
-    },
-    description: { 
-        type: String, 
-        required: true 
-    },
-    info: { 
-        type: String, 
-        required: true 
-    },
-    image:{
-        type: String,
-        validate: {
-            validator: function(url){
-                if(url.indexOf('.jpg') != -1 || url.indexOf('.png') != -1)
-                    return true;
-                else {
-                    return false;
-                }
-            }, 
-            message: "Only JPG o PNG"
-        }
+// const objectSchema = {
+//     id: { 
+//         type: Number, 
+//         required: true,
+//         unique: true
+//     },
+//     title: { 
+//         type: String, 
+//         required: true,
+//         unique: true 
+//     },
+//     price: { 
+//         type: Number, 
+//         required: true 
+//     },
+//     description: { 
+//         type: String, 
+//         required: true 
+//     },
+//     info: { 
+//         type: String, 
+//         required: true 
+//     },
+//     image:{
+//         type: String,
+//         validate: {
+//             validator: function(url){
+//                 if(url.indexOf('.jpg') != -1 || url.indexOf('.png') != -1)
+//                     return true;
+//                 else {
+//                     return false;
+//                 }
+//             }, 
+//             message: "Only JPG o PNG"
+//         }
+//     }
+// };
+// // Crear el esquema
+// const scraperSchema = mongoose.Schema(objectSchema);
+
+
+// // Crear el modelo --> Colección
+// const Scraper = mongoose.model('Eventbrite', scraperSchema);
+
+// module.exports = Scraper;
+
+async function loadData() {
+    try {
+      const jsonData = fs.readFileSync('./scrapedData2.json', 'utf8');
+      const data = JSON.parse(jsonData);
+  
+      const collection = mongoose.connection.collection('Info-conferences');
+      const result = await collection.insertMany(data);
+      console.log(`${result.insertedCount} documentos insertados.`);
+  
+      mongoose.connection.close();
+    } catch (err) {
+      console.error('Something went wrong', err);
     }
-};
-// Crear el esquema
-const scraperSchema = mongoose.Schema(objectSchema);
-
-
-// Crear el modelo --> Colección
-const Scraper = mongoose.model('Eventbrite', scraperSchema);
-
-module.exports = Scraper;
+  }
+  
+  loadData();
