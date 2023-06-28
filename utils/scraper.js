ce50d206d65eaba55c14ee6d65528abf3290b9f5
danieskka <daniescalera93@hotmail.com>
2023-06-28 11:09:23 +0200

    add/puppeteer_config

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require('fs');
+require('dotenv').config();
 
 const extracteventData = async (url,browser) => {
     try{
@@ -35,8 +36,19 @@ const scrap = async (url) => {
     try {
         const scrapedData = []
         console.log("Opening the browser......");
-        const browser = await puppeteer.launch({headless:true})
-        
+        const browser = await puppeteer.launch(
+            {   args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
+                executablePath:
+                process.env.NODE_ENV === "production"
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath()  
+        })
+      
         const page = await browser.newPage();
         await page.goto(url);
         console.log(`Navigating to ${url}...`);
